f81a3f2b5548f87fffb2a063568350772872d46d
SQCodes <paulsqmagadi@gmail.com>
2025-05-25 23:15:06 +0300

    new file:   gallery.css 	new file:   gallery.html 	new file:   images/camera/c1.png 	new file:   images/camera/c2.png 	new file:   images/camera/c3.png 	new file:   images/camera/c4.png 	new file:   images/camera/c5.png 	new file:   images/camera/pexels-alex-andrews-271121-1983037.jpg 	new file:   images/camera/pexels-madebymath-249597.jpg 	new file:   images/camera/pexels-madebymath-90946.jpg 	new file:   images/camera/pexels-marx-ilagan-1091290.jpg 	new file:   images/camera/pexels-pixabay-45889.jpg 	new file:   images/camera/pexels-pixabay-51383.jpg 	new file:   images/jenny/1.png 	new file:   images/jenny/10.jpg 	new file:   images/jenny/2.png 	new file:   images/jenny/3.png 	new file:   images/jenny/4.png 	new file:   images/jenny/5.png 	new file:   images/jenny/6.png 	new file:   images/jenny/7.png 	new file:   images/jenny/8.png 	new file:   images/jenny/9.png 	new file:   images/nature/1.jpg 	new file:   images/nature/2.jpg 	new file:   images/profile/f1.png 	new file:   images/profile/f2.png 	new file:   images/profile/f3.png 	new file:   images/profile/f4.png 	new file:   images/profile/f5.png 	new file:   images/services/art.jpg 	new file:   images/services/commercial.jpg 	new file:   images/services/fashion.jpg 	new file:   images/services/portrait.png 	new file:   images/services/travel.jpg 	new file:   images/services/wedding.jpg 	modified:   index.html 	modified:   scripts.js 	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -10,3 +10,18 @@ menuToggle.addEventListener('click', () => {
         menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
     }
 })
+
+
+const whyMeImagesContainer = document.querySelector('.why-me-images');
+let whyMeImages = document.querySelectorAll('.why-me-images img');
+let imageIndex = 0;
+showImages();
+function showImages(){
+    for (image = 0; image < whyMeImages.length; image++) {
+    whyMeImages[image].style.display = "none";
+  }
+  imageIndex++;
+  if (imageIndex > whyMeImages.length) {imageIndex = 1}
+  whyMeImages[imageIndex-1].style.display = "block";
+  setTimeout(showImages, 4000);
+}
