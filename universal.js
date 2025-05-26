c920291345dd22f9fcc9e2b0e0022f542bfa7407
SQCodes <paulsqmagadi@gmail.com>
2025-05-27 01:11:05 +0300

    modified:   index.html 	modified:   styles.css 	modified:   universal.js

--- universal.js
+++ universal.js
@@ -4,16 +4,35 @@
 // Responsive Navigation Menu
 const menuToggle = document.querySelector('.menu-toggler');
 const navMenu = document.querySelector('nav');
+const header = document.querySelector('header');
+const navLinks = document.querySelectorAll('nav a');
 
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 
     if(navMenu.classList.contains('show')) {
          menuToggle.innerHTML = '<i class="fa-solid fa-x"></i>';
+         header.style.borderBottomRightRadius = "0";
     } else {
         menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+         header.style.borderBottomRightRadius = "8px";
     }
+
 })
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+        menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    })
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === navMenu) {
+        navMenu.classList.toggle('show');
+        menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  }
+});
 
 // Scroll to Top Button
 const scrollToTopBtn = document.querySelector('.scroll-to-top');        
