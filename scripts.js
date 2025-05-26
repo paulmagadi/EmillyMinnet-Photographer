cdbac794a0111ee7467b5e1c731fed51a7840803
SQCodes <paulsqmagadi@gmail.com>
2025-05-26 18:38:59 +0300

    new file:   images/gallery/g1.jpg 	new file:   images/gallery/g2.jpg 	new file:   images/gallery/g3.jpg 	new file:   images/gallery/g4.jpg 	new file:   images/gallery/g5.jpg 	new file:   images/gallery/g6.jpg 	deleted:    images/profile/f1.png 	deleted:    images/profile/f2.png 	deleted:    images/profile/f3.png 	deleted:    images/profile/f4.png 	deleted:    images/profile/f5.png 	modified:   images/services/art.jpg 	modified:   images/services/commercial.jpg 	modified:   images/services/fashion.jpg 	new file:   images/services/portrait.jpg 	deleted:    images/services/portrait.png 	modified:   images/services/travel.jpg 	modified:   images/services/wedding.jpg 	modified:   index.html 	modified:   scripts.js 	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -12,16 +12,43 @@ menuToggle.addEventListener('click', () => {
 })
 
 
-const whyMeImagesContainer = document.querySelector('.why-me-images');
-let whyMeImages = document.querySelectorAll('.why-me-images img');
-let imageIndex = 0;
-showImages();
-function showImages(){
-    for (image = 0; image < whyMeImages.length; image++) {
-    whyMeImages[image].style.display = "none";
-  }
-  imageIndex++;
-  if (imageIndex > whyMeImages.length) {imageIndex = 1}
-  whyMeImages[imageIndex-1].style.display = "block";
-  setTimeout(showImages, 4000);
-}
+document.addEventListener('documentloaded', () => {
+    // Smooth Scrolling
+    const scrollLinks = document.querySelectorAll('a[href^="#"]');  
+    scrollLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+    // Scroll to Top Button
+    const scrollToTopBtn = document.querySelector('.scroll-to-top');        
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.classList.add('show');
+        } else {
+            scrollToTopBtn.classList.remove('show');
+        }
+    });
+});
+
+ // Why Me Images Slider
+    const whyMeImagesContainer = document.querySelector('.why-me-images');
+    let whyMeImages = document.querySelectorAll('.why-me-images img');
+    let imageIndex = 0;
+    showImages();
+    function showImages(){
+        for (image = 0; image < whyMeImages.length; image++) {
+        whyMeImages[image].style.display = "none";
+    }
+    imageIndex++;
+    if (imageIndex > whyMeImages.length) {imageIndex = 1}
+    whyMeImages[imageIndex-1].style.display = "block";
+    setTimeout(showImages, 4000);
+    }
